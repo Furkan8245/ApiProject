19358db0f723629ac722996648c3cf7e36cf5b4a
Furkan Keleş <furkan.keles.8245@hotmail.com>
2025-07-28 19:26:21 +0300

    Şef ve kategori CRUD işlemleri tanımlandı.

--- ApiProject.WebApi/Controllers/CategoriesController.cs
+++ ApiProject.WebApi/Controllers/CategoriesController.cs
@@ -15,6 +15,12 @@ namespace ApiProject.WebApi.Controllers
         {
             _context = context;
         }
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _context.Categories.ToList();
+            return Ok(values);
+        }
 
         [HttpPost]
         public IActionResult CreateCategory(Category category)
@@ -22,6 +28,28 @@ namespace ApiProject.WebApi.Controllers
             _context.Categories.Add(category);
             _context.SaveChanges();
             return Ok("Kategori ekleme işlemi başarı ile kaydedildi.");
+        }
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value= _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok("Kategori silme işlemi başarı ile kaydedildi.");
+        }
+        [HttpGet("GetCategory")]
+        public IActionResult GetCategory(int id) 
+        {
+            var value = _context.Categories.Find(id);
+            return Ok(value);
+
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Kategori güncelleme işlemi başarı ile kaydedildi.");
         }
 
     }
