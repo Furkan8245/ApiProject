dd39fa3cdae29d382d4a39d3b915e77a3dc85e24
Furkan Keleş <furkan.keles.8245@hotmail.com>
2025-08-05 19:29:30 +0300

    Doğrulama kütüphanesi olan FluemtValidation ile çalışma yapıldı.

---
+++ ApiProject.WebApi/ValidationRules/ProductValidator.cs
@@ -0,0 +1,21 @@
+﻿using ApiProject.WebApi.Entities;
+using FluentValidation;
+
+namespace ApiProject.WebApi.ValidationRules
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.ProductName).NotEmpty.WithMessage("Ürün adı boş olamaz.");
+            RuleFor(x => x.ProductName).MinimumLength(2).WithMessage("En az 2 karakter girilmesi gerekir.");
+            RuleFor(x => x.ProductName).MaximumLength(50).WithMessage("En fazla 40 karakter girilmesi gerekir.");
+
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Ürün fiyatı bilgisi girilmesi zorunludur.").LessThan(0).WithMessage("Ürün fiyatı " +
+                "negatif değer olamaz.");
+
+            RuleFor(x => x.ProductDescription).NotEmpty().WithMessage("Ürün açıklaması boş geçilemez.");
+        }
+
+    }
+}
