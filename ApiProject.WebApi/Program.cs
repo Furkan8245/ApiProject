347ffae9bb827f61c83207e544ce3b7bb2c99b1d
Furkan Keleş <furkan.keles.8245@hotmail.com>
2025-08-17 17:32:53 +0300

    Kategori tablosu güncellendi ve Api consume etme işlemi yapıldı.

--- ApiProject.WebApi/Program.cs
+++ ApiProject.WebApi/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDbContext<ApiContext>();
 builder.Services.AddScoped<IValidator<Product>, ProductValidator>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
