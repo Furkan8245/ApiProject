e8c1572cfcbcceba5a765372e9640fdab8de0a3c
Furkan Keleş <furkan.keles.8245@hotmail.com>
2025-10-29 20:04:19 +0300

    Mesaj alanı görsel açıdan düzenlendi.

--- ApiProje.WebUI/wwwroot/otika-bootstrap-admin-template/assets/js/page/contact.js
+++ ApiProje.WebUI/wwwroot/otika-bootstrap-admin-template/assets/js/page/contact.js
@@ -18,3 +18,5 @@ map.drawOverlay({
   lng: 72.532057,
   content: '<div class="popover" style="width:250px;"><div class="manual-arrow"><i class="fas fa-caret-down"></i></div><div class="popover-body"><b>Redstar</b><p><small>501, Saman Complex, Mansi Circle., <br>Gandho Road, Satellite 356987</p><p><a target="_blank" href="http://test.com">Website</a></small></p></div></div>'
 });
+
+
